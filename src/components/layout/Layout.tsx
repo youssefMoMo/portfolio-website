5ce1adc813e19c1807b3fb62ddb4d0c96beb82f1
koshoss <se7smo7amed@gmail.com>
2026-05-25 14:26:16 +0300

    fix(build): correct footer import to default layout syntax in Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode, useEffect, useRef, useState, useCallback } from "react";
 import Navbar from "./Navbar";
-import { Footer } from "./Footer";
+import Footer from "./Footer";
 import { useLocation } from "wouter";
 import { BackgroundOverlay } from "@/components/BackgroundOverlay";
 
